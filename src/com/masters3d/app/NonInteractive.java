b79ac76a64227f73718c13bef2f492ffbf4bc2c0
J Cheyo Jimenez <cheyo@masters3d.com>
2017-09-12 10:36:28 -0700

    added non interactive class

---
+++ src/com/masters3d/app/NonInteractive.java
@@ -0,0 +1,41 @@
+package com.masters3d.app;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NonInteractive {
+
+	public static void main(String[] args) {
+		System.out.println("Running automatic tests");
+		List<Integer> primesOf10 = new ArrayList<Integer>();
+		primesOf10.add(2);
+		primesOf10.add(3);
+		primesOf10.add(5);
+		primesOf10.add(7);
+		List<Integer> primesOf10Result = (new Sieve(10)).getPrimes();
+		
+		if(!primesOf10.equals(primesOf10Result)){
+			throw new RuntimeException("Sieve(10) did not return the correct result");
+		}
+		System.out.println("Testing passed for Sieve(10)");
+		for(int each : primesOf10){
+			System.out.print( each + ", ");
+		}
+		System.out.println();
+		
+		List<Integer> primesOf100 = (new Sieve(100)).getPrimes();
+		if (97 != primesOf100.get(primesOf100.size() - 1))
+		{
+			throw new RuntimeException("Sieve(10) did not return the correct result");
+
+		}
+		System.out.println("Testing passed for Sieve(100)");
+		for(int each : primesOf100){
+			System.out.print( each + ", ");
+		}
+		System.out.println();
+		System.out.println("All Tests passed");
+
+	}
+
+}
--- src/com/masters3d/app/Program.java
+++ src/com/masters3d/app/Program.java
@@ -6,39 +6,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Program {
-
-	public static void runTests(){
-		System.out.println("Running automatic tests");
-		List<Integer> primesOf10 = new ArrayList<Integer>();
-		primesOf10.add(2);
-		primesOf10.add(3);
-		primesOf10.add(5);
-		primesOf10.add(7);
-		List<Integer> primesOf10Result = (new Sieve(10)).getPrimes();
-		
-		if(!primesOf10.equals(primesOf10Result)){
-			throw new RuntimeException("Sieve(10) did not return the correct result");
-		}
-		System.out.println("Testing passed for Sieve(10)");
-		for(int each : primesOf10){
-			System.out.print( each + ", ");
-		}
-		System.out.println();
-		
-		List<Integer> primesOf100 = (new Sieve(100)).getPrimes();
-		if (97 != primesOf100.get(primesOf100.size() - 1))
-		{
-			throw new RuntimeException("Sieve(10) did not return the correct result");
-
-		}
-		System.out.println("Testing passed for Sieve(100)");
-		for(int each : primesOf100){
-			System.out.print( each + ", ");
-		}
-		System.out.println();
-		System.out.println("All Tests passed");
-			
-	}
 	
 	static public Scanner scanIn = new Scanner(System.in);
 	
@@ -65,14 +32,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Welcome to the Sieve of Erastosthenes.");
 		System.out.println("  This program calculates all the prime numbers less than or equal to the integer given.");
-		if (args.length > 1)
-		{
-			runTests();
-		} else 
-		{
-			interactive();
-		}
-		
+		interactive();
 
 	}
 
